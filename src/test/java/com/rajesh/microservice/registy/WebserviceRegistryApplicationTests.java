4b205027e20bcc30ef25fb17e7a53fa82ba3fc62
rathokal <rathokal@partnet.cisco.com>
2020-04-25 21:38:41 +0530

    initial commit

---
+++ src/test/java/com/rajesh/microservice/registy/WebserviceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajesh.microservice.registy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
